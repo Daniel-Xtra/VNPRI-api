0f9059a6307c13146dd7d6ac651e7ea5b320cb3d
Daniel <danielakanbi01@gmail.com>
2022-07-01 10:41:24 -0700

    'grg

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,15 @@ import app from "./app";
 import { Server } from "http";
 import { PORT } from "./config";
 import { logger } from "./utils/logger";
+// import { createServer } from "https";
 
 // import * as fs from "fs";
 
-// const httpsOptions = {
-//     key: fs.readFileSync("api_mypaddiapp_com.key"),
-//     cert: fs.readFileSync("api_mypaddiapp_com.crt"),
-//     ca: fs.readFileSync("api_mypaddiapp_com.ca-bundle"),
-// };
 const port = PORT || 5000;
+// const port2 = PORT2;
 
 const httpServer = new Server(app);
+// const server = createServer("", app);
 
 // app.use((req, res, next) => {
 //     console.log(req.headers, "what is it?", req.protocol, req.url);
@@ -30,6 +28,14 @@ httpServer.listen(PORT, (err) => {
 
   return logger.info(`http Server is listening on port: ${port}`);
 });
+
+// server.listen(PORT2, (err) => {
+//   if (err) {
+//     return logger.error(err);
+//   }
+
+//   return logger.info(`https Server is listening on port: ${port2}`);
+// });
 
 // new SocketController(httpServer).startSocket();
 // new SocketController(server).startSocket();
